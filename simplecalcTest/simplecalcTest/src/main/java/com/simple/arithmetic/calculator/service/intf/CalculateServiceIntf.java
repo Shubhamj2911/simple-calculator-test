8e85853814af0ec2270b3522a77d7304d9005c02
Shubhamj2911 <144548748+Shubhamj2911@users.noreply.github.com>
2023-09-09 21:31:49 +0530

    Delete simplecalcTest directory

--- simplecalcTest/simplecalcTest/src/main/java/com/simple/arithmetic/calculator/service/intf/CalculateServiceIntf.java
+++
@@ -1,9 +0,0 @@
-package com.simple.arithmetic.calculator.service.intf;
-
-import com.simple.arithmetic.calculator.models.ResponseModel;
-
-public interface CalculateServiceIntf {
-
-	public ResponseModel calculateData(int arithmeticType,int firstNumber,int secondNumber);
-	
-}
