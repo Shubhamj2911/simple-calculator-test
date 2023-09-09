c7c20b145f8d5c1579c41dce961a9ba358249eea
Shubhamj2911 <144548748+Shubhamj2911@users.noreply.github.com>
2023-09-09 21:19:48 +0530

    Simple Calculator 1,0
    Test for simple calculator

---
+++ simplecalcTest/src/test/java/com/simple/arithmetic/calculator/SimplecalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.arithmetic.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
